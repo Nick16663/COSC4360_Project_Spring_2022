0cc6525c0c48ea501b08a5a98a9b82fcb3b1e617
jmallari21 <104325926+jmallari21@users.noreply.github.com>
2022-04-26 20:45:41 -0500

    fixed edge typo
    changed "egde" to "edge"

--- Map.java
+++ Map.java
@@ -108,7 +108,7 @@ public class Map {
 			}
 		}
 
-		public void addEgde(int source, int destination, int weight) {
+		public void addEdge(int source, int destination, int weight) {
 			Edge edge = new Edge(source, destination, weight);
 			adjacencylist[source].addFirst(edge); //for directed graph
 		}
@@ -219,15 +219,15 @@ public class Map {
 		int vertices = 6;
 		Graph graph = new Graph(vertices);
 		/*
-		graph.addEgde(0, 1, 4);
-		graph.addEgde(0, 2, 3);
-		graph.addEgde(1, 3, 2);
-		graph.addEgde(1, 2, 5);
-		graph.addEgde(2, 3, 7);
-		graph.addEgde(3, 4, 2);
-		graph.addEgde(4, 0, 4);
-		graph.addEgde(4, 1, 4);
-		graph.addEgde(4, 5, 6);
+		graph.addEdge(0, 1, 4);
+		graph.addEdge(0, 2, 3);
+		graph.addEdge(1, 3, 2);
+		graph.addEdge(1, 2, 5);
+		graph.addEdge(2, 3, 7);
+		graph.addEdge(3, 4, 2);
+		graph.addEdge(4, 0, 4);
+		graph.addEdge(4, 1, 4);
+		graph.addEdge(4, 5, 6);
 		graph.printGraph();*/
 	}
 }
