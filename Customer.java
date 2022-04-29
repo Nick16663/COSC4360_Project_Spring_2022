9b85c025ff0976d5598f0ab5754b5aef049a128f
Sai Ganesh Terapalli <100099439+sterapalli@users.noreply.github.com>
2022-04-29 07:45:09 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,16 +1,40 @@
-import java.util.Scanner;
 public class Customer {
-	static void Customer() {
-	System.out.println("Name");
-	Scanner customer = new Scanner(System.in);
-	String customerName = customer.next();
-	
-	}
-	
-	static void cityName() {
-		System.out.println("City: ");
-		Scanner city = new Scanner(System.in);
-		String cityName = city.next();
-	}
-	
+    private String customerName;
+    private String streetName;
+    private String zipCode;
+    private String state;
+    private String city;
+
+    public Route(String streetName, String zipCode, String state, String city, String customerName) {
+        this.streetName = streetName;
+        this.zipCode = zipCode;
+        this.state = state;
+        this.city = city;
+        this.customerName = customerName;
+    }
+
+    private String getStreetName() {
+        return streetName;
+    }
+
+    private String getZipCode() {
+        return zipCode;
+    }
+
+    private String getState() {
+        return state;
+    }
+
+    private String getCity() {
+        return city;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String displayAddress() {
+        return "Address: " + this.streetName + " " + this.city + ", " + this.state + " " + this.zipCode;
+    }
+
 }
