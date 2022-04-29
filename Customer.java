99318b6507502019c24ac96f48fed37d0ac04599
Sai Ganesh Terapalli <100099439+sterapalli@users.noreply.github.com>
2022-04-28 23:09:04 -0500

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Customer {
+	static void Customer() {
+	System.out.println("Name");
+	Scanner customer = new Scanner(System.in);
+	String customerName = customer.next();
+	
+	}
+	
+	static void cityName() {
+		System.out.println("City: ");
+		Scanner city = new Scanner(System.in);
+		String cityName = city.next();
+	}
+	
+}
